8472f9f1d28674acfa38ea65c906314c0670fe32
Chakrasai <chakrasaiaku@gmail.com>
2025-07-13 23:55:09 +0530

    Update API endpoints to use production URLs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,13 +13,13 @@ function App() {
     console.log('Button clicked!');
 
     const pingstart = performance.now();
-    await fetch('http://localhost:3000/ping');
+    await fetch('https://speed-backend-778g.onrender.com/ping');
     const pingend = performance.now();
     setping((pingend-pingstart).toFixed(2));
 
 
     const downloadstart = performance.now();
-    const res = await fetch('http://localhost:3000/download')
+    const res = await fetch('https://speed-backend-778g.onrender.com/download')
     const blob = await res.blob();
     const downloadend = performance.now();
     const download = ((blob.size*8)/((downloadend-downloadstart)/1000)/(1024*1024)).toFixed(2);
@@ -27,7 +27,7 @@ function App() {
 
     const uplaoddata = new Uint8Array(20*1024*1024);
     const uploadstart = performance.now();
-    await fetch('http://localhost:3000/upload',{
+    await fetch('https://speed-backend-778g.onrender.com/upload',{
       method:'POST',
       body: uplaoddata
     })
